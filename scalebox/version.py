fc9da29e89ae61047c9443cb4394e5f1ace8b5ae
“baoguodong” <baogd@wangsu.com>
2026-02-03 07:36:09 +0000

    update to v1.0.0

--- scalebox/version.py
+++ scalebox/version.py
@@ -2,8 +2,8 @@
 Version information for ScaleBox Python SDK
 """
 
-__version__ = "0.1.20"
-__version_info__ = (0, 1, 20)
+__version__ = "1.0.0"
+__version_info__ = (1, 0, 0)
 
 
 def get_version() -> str:
